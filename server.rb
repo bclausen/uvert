4af8d5de971bb0de6152e76df17133c99b7c6c38
bclausen <avs@bclausen.de>
2015-12-05 12:47:29 +0100

    Alternative zu attribution.erb
    Der HTML-Text wird schon in der Route (nicht erst im View) erstellt.
    Kein Geschwindigeitsvorteil. Aber: Das was bremst, ist das Hinzufügen
    der Lehrer in die Auswahlfelder

--- server.rb
+++ server.rb
@@ -39,6 +39,48 @@ get "/attributions" do
 	erb:attributions
 end
 
+get "/attributions2" do
+	# @subject_array =[]
+	# @subjects = Subject.all
+	# @subjects.each do |subject|
+	# 	@subject_array.push(subject.name)
+	# 	@subject_array.push(subject.id) 
+	# end
+	#2. Idee: Schon fertigen HTML-Code generieren
+	@attribution_html ="<table> "
+	@schoolclasses = Schoolclass.all
+	#Hinzufürgen der Klassennamen als Überschrift
+	@schoolclasses.each do |schoolclass|
+		@attribution_html = @attribution_html + " <th> " + schoolclass.name + "</th>"
+	end
+	#Hinzufügen der Fächer
+	@subjects = Subject.all
+	@subjects.each do |subject|
+		@attribution_html = @attribution_html + "<tr> <td>" + subject.name + "<td>"
+		@schoolclasses.each do |schoolclass|
+			@subject_teachers=Department.all(:subject_id => subject.id)
+			@attribution_html = @attribution_html + "<td> <select name='" + (schoolclass.id).to_s + "_" +(subject.id).to_s + "' >"
+			#Ab hier verzögert sich das laden der Seite
+			@subject_teachers.each do |teacher|
+				@attribution_html = @attribution_html + "<option value='"+ (teacher.teacher_id).to_s + "'>" + Teacher.first(id: teacher.teacher_id).shortcut + "</option>"
+			end
+			@attribution_html = @attribution_html + "</select>"
+			#Hinzufügen der Stündigkeit (dies hat keinen Einfluss auf die Ladegeschwindigeit)
+			if Profileassignment.first(schoolclass_id: schoolclass.id) != nil then
+				@profil_id=Profileassignment.first(schoolclass_id: schoolclass.id).profile_id
+				Profilesubject.all(:profile_id => @profil_id, :subject_id => subject.id).each do |profile_subject|
+					@attribution_html = @attribution_html + profile_subject.hours.to_s
+				end
+			end
+			@attribution_html = @attribution_html + "</td>"
+		end
+		@attribution_html = @attribution_html + "</tr>"
+	end
+
+	@attribution_html = @attribution_html + " </table>"
+	erb:attributions2
+end
+
 get "/subjects" do
 	@subjects = Subject.all
 	erb:subjects
